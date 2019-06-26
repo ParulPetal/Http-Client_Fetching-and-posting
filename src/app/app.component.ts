2ceb5c690fb2fd6a62a5b4d8694edfd789a64c44
ParulPetal <parulsingh550@gmail.com>
2019-06-26 10:48:59 +0530

    hey

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'HTTP';
+ constructor(private http:HttpClient){}
+name:string="";
+email:string;
+found:boolean;
+
+onNameKeyUp(event:any)
+{
+this.name=event.target.value;
+this.found=false;
+  }
+getProfile(){
+
+  this.http.get(`https://jsonplaceholder.typicode.com/users/?name=${this.name}`)
+  .subscribe(
+    (data:any[])=>{
+//console.log(data)
+if (data.length){
+
+this.email=data[0].email;
+this.found=true;
+
+
+}
+    }
+  )
+}
+
+
+postProfile(){
+
+  this.http.post(`https://jsonplaceholder.typicode.com/users/`,
+  
+  {name:"parul",
+  email:"nohara@hotmail.com"
+}
+  )
+  .subscribe(
+    (data:any[])=>{
+console.log(data)
+
+    }
+  )
+}
+
+
+
+
+
+
+
 }
